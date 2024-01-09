101e04a2b9fb1627564a96bcf6b7b5e440068042
unknown <you@example.com>
2024-01-10 00:17:31 +0100

    second staging

--- dist/mainApp.js
+++ dist/mainApp.js
@@ -1,10 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.mainApp = void 0;
-// import auth from "./router/userRouter";
+const userRouter_1 = __importDefault(require("./router/userRouter"));
 const mainApp = (app) => {
     try {
-        // app.use("/api",auth)
+        app.use("/api", userRouter_1.default);
         app.get("/", (req, res) => {
             try {
                 // console.log(req)
---
+++ dist/model/userModel.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+;
+;
+const userModel = new mongoose_1.Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String
+    },
+    token: {
+        type: String
+    },
+    avatar: {
+        type: String
+    },
+    verify: {
+        type: Boolean
+    }
+}, { timestamps: true });
+exports.default = (0, mongoose_1.model)("users", userModel);
